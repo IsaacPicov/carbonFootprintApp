e87ae51c56b889fd817c4380fceaacb38a9e295b
Zacky <89254577+ZayLinAung@users.noreply.github.com>
2024-11-02 10:29:57 -0400

    feat: Registration Feature

--- Starter Application/app/src/main/java/com/example/b07demosummer2024/RegistrationFragment.java
+++ Starter Application/app/src/main/java/com/example/b07demosummer2024/RegistrationFragment.java
@@ -4,19 +4,74 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class RegistrationFragment extends Fragment{
 
-    private EditText firstName, lastName, email, password;
+    private EditText editFirstName, editLastName, editEmail, editPassword, editConfirmPassword;
+
+    private Button buttonRegister;
+
+    private FirebaseDatabase db;
+    private DatabaseReference itemsRef;
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
         View view = inflater.inflate(R.layout.fragment_registration, container, false);
-        firstName = view.findViewById(R.id.editTextTitle);
+        editFirstName = view.findViewById(R.id.editFirstName);
+        editLastName = view.findViewById(R.id.editLastName);
+        editEmail = view.findViewById(R.id.editEmail);
+        editPassword = view.findViewById(R.id.editPassword);
+        editConfirmPassword = view.findViewById(R.id.editConfirmPassword);
+        buttonRegister = view.findViewById(R.id.buttonUserRegister);
+
+        db = FirebaseDatabase.getInstance("https://b07finalproject-4e3be-default-rtdb.firebaseio.com/");
+
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerUser();
+            }
+        });
         return view;
+    }
+
+    private void registerUser(){
+        String firstName = editFirstName.getText().toString().trim();
+        String lastName = editLastName.getText().toString().trim();
+        String email = editEmail.getText().toString().trim();
+        String password = editPassword.getText().toString();
+        String confirmPassword = editConfirmPassword.getText().toString();
+
+        if (firstName.isEmpty() || lastName.isEmpty() || email.isEmpty() || password.isEmpty() || confirmPassword.isEmpty()) {
+            Toast.makeText(getContext(), "Please fill out all fields", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (!password.equals(confirmPassword)){
+            Toast.makeText(getContext(), "Password does not match", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        itemsRef = db.getReference("users/");
+        String id = itemsRef.push().getKey();
+        System.out.println("My id" + id);
+        Item item = new Item(id, firstName, lastName, email, password);
+
+        itemsRef.child(id).setValue(item).addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                Toast.makeText(getContext(), "Item added", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getContext(), "Failed to add item", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
