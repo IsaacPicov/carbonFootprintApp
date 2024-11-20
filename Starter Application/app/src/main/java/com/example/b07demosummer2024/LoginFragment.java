325d7ee6101c1f1ac06ce799abbb21a98f8ffdad
Frederic Tu <fredhtu@gmail.com>
2024-11-19 20:16:28 -0500

    Add forgot password functionality (mvp)

--- Starter Application/app/src/main/java/com/example/b07demosummer2024/LoginFragment.java
+++ Starter Application/app/src/main/java/com/example/b07demosummer2024/LoginFragment.java
@@ -10,33 +10,41 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import com.example.b07demosummer2024.mvp.presenters.LoginPresenter;
+import com.example.b07demosummer2024.mvp.presenters.ForgotPasswordPresenter;
 
-public class LoginFragment extends Fragment implements LoginPresenter.LoginView {
+public class LoginFragment extends Fragment implements LoginPresenter.LoginView, ForgotPasswordPresenter.ForgotPasswordView {
     private EditText editEmail, editPassword;
-    private LoginPresenter presenter;
+    
+    private LoginPresenter loginPresenter;
+    private ForgotPasswordPresenter forgotPasswordPresenter;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_login, container, false);
         editEmail = view.findViewById(R.id.editEmail);
         editPassword = view.findViewById(R.id.editPassword);
+
         Button buttonLogin = view.findViewById(R.id.buttonLogin);
         Button buttonForgotPassword = view.findViewById(R.id.buttonForgotPassword);
 
-        presenter = new LoginPresenter(this);
+        loginPresenter = new LoginPresenter(this);
+        forgotPasswordPresenter = new ForgotPasswordPresenter(this);
 
         buttonLogin.setOnClickListener(v -> {
             String email = editEmail.getText().toString().trim();
             String password = editPassword.getText().toString().trim();
-            presenter.handleLogin(email, password);
-//            Toast.makeText(getContext(), "Login successful", Toast.LENGTH_SHORT).show();
+
+            loginPresenter.handleLogin(email, password);
         });
 
         buttonForgotPassword.setOnClickListener(v -> {
             String email = editEmail.getText().toString().trim();
-            presenter.handleForgotPassword(email);
+            
+            editPassword.setText("");
+            forgotPasswordPresenter.handleForgotPassword(email);
         });
 
         return view;
@@ -55,11 +63,27 @@ public class LoginFragment extends Fragment implements LoginPresenter.LoginView 
     public void onLoginSuccess() {
         Toast.makeText(getContext(), "Login successful", Toast.LENGTH_SHORT).show();
 
-        // move back to the homepage somehow
+        // redirect back to homepage
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+        fragmentManager.popBackStack();
+
+        // clear inputs
+        editEmail.setText("");
+        editPassword.setText("");
     }
 
     @Override
     public void onLoginFail(String message) {
+        Toast.makeText(getContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onForgotPasswordSuccess() {
+        Toast.makeText(getContext(), "Password reset email sent", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onForgotPasswordFail(String message) {
         Toast.makeText(getContext(), message, Toast.LENGTH_SHORT).show();
     }
 }
